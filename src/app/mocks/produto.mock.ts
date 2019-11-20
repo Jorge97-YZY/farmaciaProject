a546cacf7dc76eb1d78459a07512d2f187c8d618
Jorge Arsénio Manuel <jorgebc15@live.com>
2019-11-20 19:29:36 +0100

    Codigo da mock Produto alterado

--- src/app/mocks/produto.mock.ts
+++ src/app/mocks/produto.mock.ts
@@ -2,5 +2,6 @@ import { Produto } from './../model/produto.model';
 
 export const PRODUTO: Produto[] = [
     { id: 1, descricao: 'Paracitamol 250ml', quantidade: 10, preco: 250, fornecedor: { id: 4, nome: 'LuFarm', telefone: 929635271, email: 'farmlub@gmail.com', nif: '73476HA02', status: true } },
+    { id: 2, descricao: 'Dipirona 250ml', quantidade: 100, preco: 150, fornecedor: {id: 3, nome: 'LDAF', telefone: 910195723, email: 'ldaf@hotmail.com', nif: '73476HA02', status: false} }
 
 ];
